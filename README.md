4eb89fc888d38873225452e9e3800513ad888f03
drlight17 <37434652+drlight17@users.noreply.github.com>
2024-04-05 19:22:13 +0300

    First version 0.1
    First version 0.1

--- README.md
+++ README.md
@@ -1,30 +1,37 @@
-# Roundcube + Nextcloud with universal SSO instruction (not only login + password based NC authorization but also passwordless like SSO).
-In order to use this you will need:
+# Roundcube Webmail iframe SSO connector plugin
+
+To successfully use this plugin you will need:
 - configure [dovecot master password](https://doc.dovecot.org/configuration_manual/authentication/master_users/) on your mail server
 - [dovecot_impersonate plugin](https://github.com/corbosman/dovecot_impersonate) in your Roundcube instance
-- roundcube must be [allowed to be embedded](https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy) into iframe from your nextcloud
-- [php-jwt](https://github.com/firebase/php-jwt) in your roundcube instance
-- nc-login.php from this repo
-- [nextcloud external sites plugin](https://apps.nextcloud.com/apps/external) installed and properly configured
+- roundcube must be [allowed to be embedded](https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy) into iframe on your server
+- (OPTIONAL)[nextcloud external sites plugin](https://apps.nextcloud.com/apps/external) installed and properly configured
 
 ## Step by step guide:
 1. Configure master login, password and seperator in your dovecot config
 2. Install and configure dovecot_impersonate plugin with the same separator from dovecot config
-3. (!) Configure your roundcube web server or reverse proxy to allow embedding roundcube into nextcloud iframe (origin policies)
-4. Place nc-login.php to the root dir of your roundcube instance
-5. Edit nc-login.php config section, add your dovecot master password attributes and public key from nextcloud external app (use command below to get the key):
+3. (OPTIONAL)Configure your roundcube web server or reverse proxy to allow embedding roundcube into your server iframe (origin policies)
+4. Place plugin to the plugins dir of your roundcube instance
+```
+# cd /rouncude_path/plugins
+# git clone https://github.com/drlight17/iframed_login_sso 
+```
+5. Edit plugin config file to your needs:
+```
+# cp /rouncude_path/plugins/iframed_login_sso/config.inc.php.dist /rouncude_path/plugins/iframed_login_sso/config.inc.php
+# vi /rouncude_path/plugins/iframed_login_sso/config.inc.php
+```
+6. (OPTIONAL) To get the key from  nextcloud external app use command below:
 ```
 sudo -u www-user /path/to/your/nextcloud/occ config:app:get external jwt_token_pubkey_es256
 ```
-7. Place php-jwt in the root dir of your roundcube instance (you also could use composer for this but it will require some appropriate nc-login.php source code changes):
-```
-# cd /rouncude_path
-# git clone https://github.com/firebase/php-jwt
-```
-6. Install external nextcloud app and add this url as external site:
-`https://yourmailserver.address/nc-login.php?jwt={jwt}`
+7. Place "iframed_login_sso" in your roundcube config file in plugins array variable
+8. Use this url for iframe (!replace jwt_req_parameter with your set parameter in the plugin config.inc.php)
+`https://yourroundcubeserver.address/?_autologin=yes&jwt_req_parameter=formed_jwt_token`
+(OPTIONAL) For nextcloud external app use url below (with {jwt} placeholder, you can read more about it in the nextcloud external sites app settings menu)
+`https://yourroundcubeserver.address/?_autologin=yes&jwt_req_parameter={jwt}`
 
 ---
-This could be used as a source for the new Roundcube plugin. I just have no time to build one for now.
+[php-jwt](https://github.com/firebase/php-jwt) is used for jwt tokens decode functionality. 
+All rights belong to their owners.
 
 
---
+++ iframed_login_sso.php
@@ -0,0 +1,178 @@
+<?php
+
+/**
+ * Roundcube Webmail iframe SSO connector 
+ * This performs an automatic login if jwt token is taken by POST or GET request
+ *
+ * @license GNU GPLv3+
+ * @author Samoilov Your aka drlight
+ * @version 0.1
+ */
+
+// git clone https://github.com/firebase/php-jwt 
+
+require_once 'php-jwt/src/Key.php';
+require_once 'php-jwt/src/JWTExceptionWithPayloadInterface.php';
+require_once 'php-jwt/src/ExpiredException.php';
+require_once 'php-jwt/src/SignatureInvalidException.php';
+require_once 'php-jwt/src/BeforeValidException.php';
+require_once 'php-jwt/src/JWT.php';
+
+use Firebase\JWT\Key;
+use Firebase\JWT\JWTExceptionWithPayloadInterface;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\SignatureInvalidException;
+use Firebase\JWT\BeforeValidException;
+use Firebase\JWT\JWT;
+
+
+class iframed_login_sso extends rcube_plugin
+{
+
+
+    public $task = 'login|logout|settings|mail';
+
+    /**
+     * Plugin initialization
+     */
+    public function init()
+    {
+
+        // load config
+        $this->load_config('config.inc.php.dist');
+        $this->load_config('config.inc.php');
+        $RCMAIL = rcmail::get_instance(0, $GLOBALS['env']);
+
+        // get webmail Location URL from $_SERVER
+
+        $cur_URL = (empty($_SERVER['HTTP_X_FORWARDED_PROTO']) ? empty($_SERVER['HTTPS']) ? 'http' : 'https' : 'https')  . "://".$_SERVER['HTTP_HOST'];
+
+        // check if RC is opened in iframe
+        if( isset($_SERVER['HTTP_SEC_FETCH_DEST']) && $_SERVER['HTTP_SEC_FETCH_DEST'] == 'iframe' ) {
+            $iframed = true;
+        } else {
+            $iframed = false;
+        }
+
+        if (isset($_COOKIE["autologin"])&&($RCMAIL->config->get('hide_logout'))&&$iframed) {
+            $this->add_hook('ready', [$this, 'hide_logout']);
+        }
+
+        if (isset($_COOKIE["autologin"])&&($RCMAIL->config->get('show_default_logo'))&&$iframed) {
+            $this->add_hook('ready', [$this, 'show_default_logo']);
+        }
+
+        // init application, start session, init output class, etc.
+        $this->add_hook('startup', [$this, 'startup']);
+        $this->add_hook('authenticate', [$this, 'authenticate']);
+        $this->add_hook('logout_after', [$this, 'logout']);
+
+    }
+
+    /**
+     * 'startup' hook handler
+     *
+     * @param array $args Hook arguments
+     *
+     * @return array Hook arguments
+     */
+    function startup($args)
+    {
+        $RCMAIL = rcmail::get_instance(0, $GLOBALS['env']);
+        if (!empty($_SESSION['user_id']) && !empty($_REQUEST['_autologin']) ) {
+            // purge the session in case of new login when a session already exists
+            $RCMAIL->kill_session();
+        }
+        // change action to login
+        if (empty($_SESSION['user_id']) && !empty($_REQUEST['_autologin']) ) {
+            $args['action'] = 'login';
+        }
+        return $args;
+    }
+
+    /**
+     * 'authenticate' hook handler
+     *
+     * @param array $args Hook arguments
+     *
+     * @return array Hook arguments
+     */
+    function authenticate($args)
+    {
+        // include environment
+        require_once 'program/include/iniset.php';
+        $RCMAIL = rcmail::get_instance(0, $GLOBALS['env']);
+        $request_valid = !empty($_SESSION['temp']) && $RCMAIL->check_request();
+        // Get from config
+        $this->dovecot_impersonate_login = $RCMAIL->config->get('dovecot_impersonate_login');
+        $this->dovecot_impersonate_separator = $RCMAIL->config->get('dovecot_impersonate_separator');
+        $this->dovecot_impersonate_password = $RCMAIL->config->get('dovecot_impersonate_password');
+        $this->key = $RCMAIL->config->get('key');
+        $this->leeway = $RCMAIL->config->get('leeway');
+        $this->jwt_req_parameter = $RCMAIL->config->get('jwt_req_parameter');
+        $this->jwt_login_claim = $RCMAIL->config->get('jwt_login_claim');
+
+        Firebase\JWT\JWT::$leeway = $this->leeway;
+
+        if (!empty($_REQUEST['_autologin'])) {
+
+            $jwt = rawurldecode($_REQUEST[$this->jwt_req_parameter]);
+
+            // dovecot impersonate plugin process
+
+            try {
+                    $keyO = new Key($this->key, 'ES256');
+                    $decoded = JWT::decode($jwt, $keyO);
+                    //print_r($decoded);
+            } catch (\Throwable $e) {
+                    echo 'Error: "'.$e->getMessage().'"';
+                    exit;
+            }
+
+            
+
+            $args['user']        = get_object_vars(get_object_vars($decoded)['userdata'])[$this->jwt_login_claim].$this->dovecot_impersonate_separator.$this->dovecot_impersonate_login;
+            $args['pass']        = $this->dovecot_impersonate_password;
+            $args['host']        = $RCMAIL->autoselect_host();
+            $args['cookiecheck'] = true;
+            $args['valid']       = $request_valid;
+
+            setcookie ('autologin','YES',time()+3600);
+        }
+
+        return $args;
+    }
+
+    /**
+     * remove autologin cookie
+     *
+     * @return nothing
+     */
+    function logout($args)
+    {
+        setcookie ('autologin','',time()-3600);
+    }
+
+    /**
+     * hides logout button
+     *
+     * @return nothing
+     */
+    function hide_logout()
+    {
+        $RCMAIL = rcmail::get_instance(0, $GLOBALS['env']);
+        $RCMAIL->config->set('disabled_actions', 'logout');
+        
+    }
+    /**
+     * shows default logo
+     *
+     * @return nothing
+     */
+    function show_default_logo()
+    {
+        $RCMAIL = rcmail::get_instance(0, $GLOBALS['env']);
+        $RCMAIL->config->set('skin_logo', null);
+        
+    }
+}
